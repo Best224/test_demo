732fdb5e8aa99f5b5940589a9fd3affb685689df
Best <Best@gmail.com>
2022-09-15 17:50:01 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,8 @@
+class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test1");
+        System.out.println("git test2");
+        System.out.println("git test3");
+    }
+}
+
