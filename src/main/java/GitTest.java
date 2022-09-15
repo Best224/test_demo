9f48b60b03028e7a60bafcd11d9e1be6a9f574c9
Best <Best@gmail.com>
2022-09-15 19:00:43 +0800

    third commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,6 +4,8 @@ class GitTest {
         System.out.println("git test2");
         System.out.println("git test3");
         System.out.println("git test4");
+        System.out.println("fuck you hot-fix");
+
     }
 }
 
