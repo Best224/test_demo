cb829ab070974d7f62dee3e4535159ab0e3393dd
Best <Best@gmail.com>
2022-09-15 18:59:41 +0800

    hot-fix commit1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,6 +4,7 @@ class GitTest {
         System.out.println("git test2");
         System.out.println("git test3");
         System.out.println("git test4");
+        System.out.println("hot-fix test!");
     }
 }
 
